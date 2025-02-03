85d3e52eebc16d59799f80835863ad564b7920d0
nandanaraju <nandanav0074@gmail.com>
2025-02-03 13:24:07 +0530

    modified

--- app.js
+++ app.js
@@ -2,30 +2,25 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// Import Routes
 const userRoutes = require('./routes/userRoutes');
 const groupRoutes = require('./routes/groupRoutes');
 const notificationRoutes = require('./routes/notificationRoutes');
 
-// Load environment variables
 dotenv.config();
 
-// Initialize Express app
 const app = express();
 app.use(express.json());
 
-// Routes
 app.use('/api', userRoutes);
 app.use('/api', groupRoutes);
 app.use('/api', notificationRoutes);
 
-// Database Connection
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI, {})
     .then(() => {
-        console.log("✅ MongoDB Connected");
-        app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+        console.log(" MongoDB Connected");
+        app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
     })
-    .catch(err => console.error("❌ Database connection error:", err));
+    .catch(err => console.error("Database connection error:", err));
